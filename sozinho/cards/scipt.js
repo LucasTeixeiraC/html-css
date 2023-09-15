9e799c262eaa2780a247fc4ad8d6a447fb4c5e3a
LucasTeixeiraC <uchih43@gmail.com>
2023-09-15 21:00:45 +0100

    new

---
+++ sozinho/cards/scipt.js
@@ -0,0 +1,21 @@
+
+
+function next() {
+   const img = document.getElementById('figura')
+   img.src = "assetz/nootbook2.png";
+   
+}
+function back (){
+   const img = document.getElementById('figura')
+   img.src = "assetz/nootbook1.png"
+}
+
+function next2() {
+   const img2 = document.getElementById('img2')
+   img2.src = "assetz/iphoneazul3.png"
+}
+function back2(){
+   const img2 = document.getElementById('img2')
+   img2.src = "assetz/iphoneazul1.png";
+   
+}
