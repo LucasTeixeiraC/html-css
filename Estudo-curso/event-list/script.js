5a3370d808ee078323f3beca19a3366094f2c8e3
LucasTeixeiraC <uchih43@gmail.com>
2023-10-14 12:24:20 +0100

    Create script.js

---
+++ Estudo-curso/event-list/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('submit' , function ( evento ) {
+    evento.preventDefault();
+
+    var n1 = document.querySelector('#n1').value;
+    var n2 = document.querySelector('#n2').value;
+    var n3 = document.querySelector('#n3').value;
+    var n4 = document.querySelector('#n4').value;
+
+    let media = (n1+n2+n3+n4) / 4
+
+    
+
+    })
