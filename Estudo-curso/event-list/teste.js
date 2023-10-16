4e868e8e4303f7ad4b815d6cc34608ca46764686
LucasTeixeiraC <uchih43@gmail.com>
2023-10-16 22:53:40 +0100

    Create teste.js

---
+++ Estudo-curso/event-list/teste.js
@@ -0,0 +1,26 @@
+/* let nome = 'lucas';
+
+switch (nome) {
+    case 'Lucas', 'lucas':
+    
+        console.log("Vi aqui que seu nome é Lucas");
+        break;
+    default:
+        console.log("Desculpa, eu só trabalho com o Lucas");
+        break;
+} */ 
+
+/* let numero = 8
+console.log("Vejo que voce escolheu a tabuada do " + numero + " aqui esta ela ")
+
+ for (i = 0; i < 11; i++) {
+    console.log(numero + " X " + i + " = " + numero*i )
+} */
+
+/* function soma(n1, n2) {
+    let resultado1 = n1 + n2;
+    return resultado1
+}
+
+let resultado2 = soma(15, 20)
+console.log(resultado2) */
