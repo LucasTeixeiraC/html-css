b790e022fb7c1ba945f85d8162e4e4eabb6b0e65
LucasTeixeiraC <uchih43@gmail.com>
2023-10-16 23:11:11 +0100

    Update script.js

--- Estudo-curso/validador-cpf/script.js
+++ Estudo-curso/validador-cpf/script.js
@@ -1,3 +1,18 @@
+
+function validCpf (cpf) {
+    return true;
+}
+
+
 function validar() {
     console.log('iniciando valição de cpf')
+    let cpf = document.getElementById('cpf_digitado').value
+
+    let resultadoValidacao = validCpf (cpf);
+
+    if (resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
 }
